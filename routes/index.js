7bab6b78707313079755d02d6deafaf164678173
Tinkerbell <tino.krenckel@gmail.com>
2020-05-05 17:32:14 +0200

    add uploads folder to ignore list and add recently added on main page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@
 const express = require("express") // because we use express for the entire application
 const router = express.Router() // router partion 
 
+/* Import the books-module which is containing the handler for books */
+const Book = require("../models/book")
+
 /* handle the routes req is the actuall request of the route and res is the result we're sending back */
-router.get("/", (req, res) => {
-    res.render("index") // inputted the content of index.ejs in views folder into the layouts.ejs html (layout)
+router.get("/", async (req, res) => {
+    let books
+
+    try {
+        books = await Book.find().sort({ createAt: "desc" }).limit(10).exec()
+    } catch (error) {
+        books = []
+    }
+    res.render("index", {
+        books: books
+    }) // inputted the content of index.ejs in views folder into the layouts.ejs html (layout)
 
 }) // get the very rout of our application (same like :3000)
 
