ea6679d7eee62360bd8a113bd5b3cb58b3746d6c
Tinkerbell <tino.krenckel@gmail.com>
2020-05-09 17:37:50 +0200

    Adding filepond library from web for imaged

--- routes/books.js
+++ routes/books.js
@@ -55,12 +55,39 @@ router.get("/new", async (reg, res) => {
     renderNewPage(res, new Book())
 })
 
+// show book by id
+router.get("/:id", async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+            .populate("author") //populate attempt to include all the object parameters inside call
+            .exec() 
+        res.render("books/show", {
+            book: book
+        })
+    } catch (error) {
+        console.log(error);
+        
+        res.redirect("/")
+    }
+})
+
+// edit book by id
+router.get("/:id/edit", async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    } catch (error) {
+        res.redirect("/")
+    }
+    
+    
+    
+})
 
 // route to create a new Book using post command  (rest) for creation
 router.post("/", upload.single('cover'), async (req, res) => {
     const fileName = req.file != null ? req.file.filename : null // req.file is inserted by upload function which is uploading the file with "multer" package
 
-    console.log("fileName:" + fileName);
     /* creating a book constant and fill it with the values in request body */
     const book = new Book({
         title: req.body.title,
@@ -70,20 +97,43 @@ router.post("/", upload.single('cover'), async (req, res) => {
         coverImageName: fileName,
         description: req.body.description
     })
-    console.log("book:" + book);
     
     /* saving the new created book */
     try {
         const newBook = await book.save()
-        //res.redirect(`books/${newAuthor.id}`) // with ` ` also variables can be included `${variable.name}`
-        res.redirect("books") // only because the page for a specific book isn't created    
+        res.redirect(`books/${book.id}`) // with ` ` also variables can be included `${variable.name}`
     } catch (error) {
         if (book.coverImageName != null) {
             removeBookCover(book.coverImageName) // if an error appear but the file was created, delete the file
         }        
-        renderNewPage(res, book, true)
+        renderNewPage(res, book,"new", true)
     }
 })
+
+// update book route
+router.put("/:id", upload.single('cover'), async (req, res) => {
+    let book
+    
+    /* saving the new created book */
+    try {
+        book = await book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.authorid
+        book.publishDate = new Date(req.body.publishDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+        if (req.body.cover != null && req.body.cover !== "") {
+            saveCover(book, req.body.cover)
+        }
+        res.redirect(`/books/${book.id}`) // with ` ` also variables can be included `${variable.name}`
+    } catch (error) {
+        if (book.coverImageName != null) {
+            removeBookCover(book.coverImageName) // if an error appear but the file was created, delete the file
+        }        
+        renderNewPage(res, book,"new", true)
+    }
+})
+
 function removeBookCover(fileName){
     fs.unlink(path.join(uploadPath, fileName), err => {
         if (err) {
@@ -92,7 +142,16 @@ function removeBookCover(fileName){
     })
 }
 
+
 async function renderNewPage(res, book, hasError = false){
+    renderFormPage(res, book, "new", hasError)
+}
+
+async function renderEditPage(res, book, hasError = false){
+    renderFormPage(res, book, "edit", hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false){
     try {
         const authors = await Author.find({})
         const params = {
@@ -103,7 +162,7 @@ async function renderNewPage(res, book, hasError = false){
         if (hasError) {
             params.errorMessage = "Error creating Book"
         }
-        res.render("books/new", params) // render books/new.ejs view and send objects within 
+        res.render(`books/${form}`, params) // render books/new.ejs view and send objects within 
     } catch (error) {
         res.redirect("/books")
     }
