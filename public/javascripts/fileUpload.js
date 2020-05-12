dd14925ffa93d2b82a0d4de6f12d8aa63f750f14
Tinkerbell <tino.krenckel@gmail.com>
2020-05-12 13:03:28 +0200

    ading picture size into filePond style in fileUpload.js

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,27 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
+/* to use variables from stylesheets, we have to check for variables */
+const rootStyles = window.getComputedStyle(document.documentElement); /* get all styles defined in root tag of styles  */
+if (rootStyles.getPropertyValue("--book-cover-width-large") != null && rootStyles.getPropertyValue("--book-cover-width-large") !== "") {
+    ready()
+} else {/* if variables currently not available load them by calling tha main.css file via id */
+    document.getElementById("main-css")
+        .addEventListener("load", ready);
+}
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue("--book-cover-width-large"));
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue("--book-cover-aspect-ratio"));
+    const coverHeight = coverWidth / coverAspectRatio;
+
+    /* define filePond settings */
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    )
+    FilePond.setOptions({    
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+}
 FilePond.parse(document.body);
